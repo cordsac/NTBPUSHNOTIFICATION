8e9bd77c060c677c7ef39b8e9444f1e450594b83
sachithw <sachithw@JKIT-SACHITHWI>
2022-03-19 23:15:12 +0530

    Add project files.

---
+++ NTBPUSHNOTIFICATION/NTBPUSHNOTIFICATION.Android/FirebaseMessagingService.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NTBPUSHNOTIFICATION.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            var body = message.GetNotification().Body;
+            //base.OnMessageReceived(p0);
+        }
+    }
+}
